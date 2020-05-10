d1d8286289507e4530d256dc7b53fdae6ffbe8ac
a.fedotov <a.fedotov@haulmont.com>
2020-05-10 19:07:11 +0400

    add backup filestorage

--- backup_psql_files.py
+++ backup_psql_files.py
@@ -5,7 +5,7 @@ import sys
 import datetime
 import os
 import subprocess
-
+import tarfile
 
 if len(sys.argv) > 1:
     user_bk = sys.argv[1]
@@ -34,13 +34,13 @@ backup_file_dbname = f'{db_name}-{current_date}.backup'
 
 
 home_user = f'/home/{user_bk}'
-USER_WWW=f'{home_user}/www'
+user_www = f'{home_user}/www'
 exclude_dir = "site/media/CACHE"
 
 
 keep_backup_day = 10
 keep_backup_yesterday = 1
-FS_ZIP = f'{backup_path}/files-{current_date}.tar.gz'
+fs_zip = f'{backup_path}/files-{current_date}.tar.gz'
 
 log_dir = f'{home_user}/_log'
 log = f'{log_dir}/{script_name}.log'
@@ -55,4 +55,18 @@ if not os.path.exists(backup_path):
 
 pg_dump = f'pg_dump --verbose -h {db_host} -F c -U {db_user} -f {backup_path}/{backup_file_dbname} {db_name}'
 pg_dump_run = subprocess.run(pg_dump, shell=True, stderr=subprocess.PIPE, encoding='utf-8')
-print(pg_dump_run.stderr)
+# write output pg_dump into file
+with open(logerr, 'w') as logerr_file:
+    logerr_file.writable(pg_dump_run.stderr)
+print(f'{timestamp()} Backup db {db_name} complete!')
+
+print(f'{timestamp()} Backup filestorage {fs_zip} complete!')
+
+
+def excludes_fn(name):
+    return exclude_dir in name
+
+
+with tarfile.open(fs_zip, 'w:gz') as tar:
+    tar.add(user_www, exclude=excludes_fn)
+
