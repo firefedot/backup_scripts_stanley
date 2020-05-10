ef0977b0f5acf5b0860da6d954babf6d1b6af23a
a.fedotov <a.fedotov@haulmont.com>
2020-05-10 18:07:09 +0400

    fix output

--- backup_psql_files.py
+++ backup_psql_files.py
@@ -6,6 +6,7 @@ import datetime
 import os
 import subprocess
 
+
 if len(sys.argv) > 1:
     user_bk = sys.argv[1]
     if user_bk == 'srisovki':
@@ -23,12 +24,13 @@ def timestamp():
 
 print(f'{timestamp()} Begin backup process for {user_bk} , db_name: {db_name}, db_user: {db_user}, {datetime.datetime.now()} ')
 
+script_name = os.path.basename(__file__).split('.')[0]
 home_path = '/home/stanley'
 db_host = 'localhost'
 db_port = 5432
 current_date = datetime.date.today().strftime('%Y-%m-%d')
 backup_path = f'{home_path}/yandex/backup/{user_bk}'
-backup_file_dbname=f'{db_name}-{current_date}.backup'
+backup_file_dbname = f'{db_name}-{current_date}.backup'
 
 
 home_user = f'/home/{user_bk}'
@@ -40,8 +42,9 @@ keep_backup_day = 10
 keep_backup_yesterday = 1
 FS_ZIP = f'{backup_path}/files-{current_date}.tar.gz'
 
-log = ''
-logerr = ''
+log_dir = f'{home_user}/_log'
+log = f'{log_dir}/{script_name}.log'
+logerr = f'{log_dir}/{script_name}_error.log'
 
 print(f'{timestamp()} Backup Database: {db_name}, userdb: {db_user}')
 print(f'{timestamp()} Create {backup_path}')
@@ -51,13 +54,4 @@ if not os.path.exists(backup_path):
     print(f'{timestamp()} Create folder {backup_path} - complete')
 
 pg_dump = f'pg_dump --verbose -h {db_host} -F c -U {db_user} -f {backup_path}/{backup_file_dbname} {db_name}'
-pg_dump_run = subprocess.run(pg_dump, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-
-# with gzip.open(‘backup.gz’, ‘wb’) as f:
-#     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-#
-#     for stdout_line in iter(popen.stdout.readline, “”):
-#         f.write(stdout_line.encode(‘utf - 8’))
-#
-#         popen.stdout.close()
-#         popen.wait()
+pg_dump_run = subprocess.run(pg_dump, shell=True, stderr=logerr, encoding='utf-8')
